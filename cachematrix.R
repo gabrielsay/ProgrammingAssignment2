15097d2cb82e338bc18f5743fd16ae8761dc277f
GabrielSay <gsay617@gmail.com>
2019-03-30 17:32:51 -0700

    code for makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## To practice enclosing objects
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <-function() x
+  setInverse <- function() inv <<- solve(x)
+  getInverse <- function() inv
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
